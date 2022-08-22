bee0ccfc8bdb3095d6f90118061fabe35db3a96a
Alberto Chiappa <alberto.chiappa@epfl.ch>
2022-08-22 09:52:17 +0200

    Add custom baoding env and some utilities for future env enhancements

---
+++ src/envs/__init__.py
@@ -0,0 +1,24 @@
+import gym
+import os
+import myosuite
+import numpy as np
+from definitions import ROOT_DIR
+
+
+myosuite_path = (
+    os.path.join(ROOT_DIR, "data", "myosuite")
+)
+
+# MyoChallenge Baoding: Phase1 env
+gym.envs.registration.register(
+    id="CustomMyoChallengeBaodingP1-v1",
+    entry_point="src.envs.baoding:CustomBaodingEnv",
+    max_episode_steps=200,
+    kwargs={
+        "model_path": myosuite_path + "/assets/hand/myo_hand_baoding.mjb",
+        "normalize_act": True,
+        # 'goal_time_period': (5, 5),
+        "goal_xrange": (0.025, 0.025),
+        "goal_yrange": (0.028, 0.028),
+    },
+)
