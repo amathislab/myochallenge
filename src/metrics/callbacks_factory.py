7be6cf80add510db91efb012bfa8287068ac3cc1
Alberto Chiappa <alberto.chiappa@epfl.ch>
2022-08-22 09:53:24 +0200

    Create callback functions for rllib

---
+++ src/metrics/callbacks_factory.py
@@ -0,0 +1,21 @@
+from ray.rllib.agents.callbacks import DefaultCallbacks
+from metrics.callbacks import FullInfoCallbacks
+from metrics.callbacks import MyoInfoCallbacks
+
+class CallbacksFactory:
+    """Static factory to get a callback class by name. Returns the default
+    callbacks class if it receives None.
+    
+    Raises:
+        ValueError: when the input string does not correspond to a know callback.
+    """
+    @staticmethod
+    def get_callbacks_class(name):
+        if name is None:
+            return DefaultCallbacks
+        elif name == "FullInfoCallbacks":
+            return FullInfoCallbacks
+        elif name == "MyoInfoCallbacks":
+            return MyoInfoCallbacks
+        else:
+            raise ValueError("Unknown callbacks name: ", name)
