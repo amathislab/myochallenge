1bef5bd9dcb21a5e7ca183c22e72b64ac78564d7
Alberto Chiappa <alberto.chiappa@epfl.ch>
2022-09-21 14:17:55 +0200

    Update baoding env to accept different tasks and rsi from config

--- src/envs/baoding.py
+++ src/envs/baoding.py
@@ -1,88 +1,9 @@
 import collections
-
 import numpy as np
+import random
 from myosuite.envs.myo.myochallenge.baoding_v1 import BaodingEnvV1
-
-
-class CustomBaodingEnv(BaodingEnvV1):
-    DEFAULT_RWD_KEYS_AND_WEIGHTS = {
-        "pos_dist_1": 5.0,
-        "pos_dist_2": 5.0,
-        "alive": 0.0,
-        "act_reg": 0.0,
-    }
-
-    def get_reward_dict(self, obs_dict):
-        # tracking error
-        target1_dist = np.linalg.norm(obs_dict["target1_err"], axis=-1)
-        target2_dist = np.linalg.norm(obs_dict["target2_err"], axis=-1)
-        target_dist = target1_dist + target2_dist
-        act_mag = (
-            np.linalg.norm(self.obs_dict["act"], axis=-1) / self.sim.model.na
-            if self.sim.model.na != 0
-            else 0
-        )
-
-        # detect fall
-        object1_pos = (
-            obs_dict["object1_pos"][:, :, 2]
-            if obs_dict["object1_pos"].ndim == 3
-            else obs_dict["object1_pos"][2]
-        )
-        object2_pos = (
-            obs_dict["object2_pos"][:, :, 2]
-            if obs_dict["object2_pos"].ndim == 3
-            else obs_dict["object2_pos"][2]
-        )
-        is_fall_1 = object1_pos < self.drop_th
-        is_fall_2 = object2_pos < self.drop_th
-        is_fall = np.logical_or(is_fall_1, is_fall_2)  # keep both balls up
-
-        rwd_dict = collections.OrderedDict(
-            (
-                # Perform reward tuning here --
-                # Update Optional Keys section below
-                # Update reward keys (DEFAULT_RWD_KEYS_AND_WEIGHTS) accordingly to update final rewards
-                # Examples: Env comes pre-packaged with two keys pos_dist_1 and pos_dist_2
-                # Optional Keys
-                ("pos_dist_1", -1.0 * target1_dist),
-                ("pos_dist_2", -1.0 * target2_dist),
-                ("alive", ~is_fall),
-                # Must keys
-                ("act_reg", -1.0 * act_mag),
-                ("sparse", -target_dist),
-                (
-                    "solved",
-                    (target1_dist < self.proximity_th)
-                    * (target2_dist < self.proximity_th)
-                    * (~is_fall),
-                ),
-                ("done", is_fall),
-            )
-        )
-        rwd_dict["dense"] = np.sum(
-            [wt * rwd_dict[key] for key, wt in self.rwd_keys_wt.items()], axis=0
-        )
-
-        # Sucess Indicator
-        self.sim.model.geom_rgba[self.object1_gid, :2] = (
-            np.array([1, 1])
-            if target1_dist < self.proximity_th
-            else np.array([0.5, 0.5])
-        )
-        self.sim.model.geom_rgba[self.object2_gid, :2] = (
-            np.array([0.9, 0.7])
-            if target1_dist < self.proximity_th
-            else np.array([0.5, 0.5])
-        )
-
-        return rwd_dict
-
-
-import collections
-
-import numpy as np
-from myosuite.envs.myo.myochallenge.baoding_v1 import BaodingEnvV1
+from myosuite.envs.myo.base_v0 import BaseV0
+from myosuite.envs.myo.myochallenge.baoding_v1 import Task, WHICH_TASK
 
 
 class CustomBaodingEnv(BaodingEnvV1):
@@ -160,10 +81,12 @@ class CustomBaodingEnv(BaodingEnvV1):
         return rwd_dict
 
     def reset(self, reset_pose=None, reset_vel=None, reset_goal=None, time_period=None):
-
-        # MODIFICATION: randomnize starting target position along the cycle
-        # random_phase = np.random.uniform(low=-np.pi, high=np.pi)
-        random_phase = 0
+        
+        if self.rsi:
+            # MODIFICATION: randomnize starting target position along the cycle
+            random_phase = np.random.uniform(low=-np.pi, high=np.pi)
+        else:
+            random_phase = 0
         self.ball_1_starting_angle = 3.0 * np.pi / 4.0 + random_phase
         self.ball_2_starting_angle = -1.0 * np.pi / 4.0 + random_phase
 
@@ -202,3 +125,79 @@ class CustomBaodingEnv(BaodingEnvV1):
         self.set_state(qpos, qvel)
 
         return self.get_obs()
+
+    def _setup(
+        self,
+        frame_skip: int = 10,
+        drop_th=1.25,  # drop height threshold
+        proximity_th=0.015,  # object-target proximity threshold
+        goal_time_period=(5, 5),  # target rotation time period
+        goal_xrange=(0.025, 0.025),  # target rotation: x radius (0.03)
+        goal_yrange=(0.028, 0.028),  # target rotation: x radius (0.02 * 1.5 * 1.2)
+        obs_keys: list = BaodingEnvV1.DEFAULT_OBS_KEYS,
+        weighted_reward_keys: list = DEFAULT_RWD_KEYS_AND_WEIGHTS,
+        task=None,
+        enable_rsi=False,  # random state init
+        **kwargs
+    ):
+
+        # user parameters
+        if task is None:
+            self.which_task = Task(WHICH_TASK)
+        else:
+            if task == "cw":
+                self.which_task = Task(Task.BAODING_CW)
+            elif task == "ccw":
+                self.which_task = Task(Task.BAODING_CCW)
+            elif task == "random":
+                self.which_task = Task(random.choice(list(Task)))
+            else:
+                raise ValueError("Unknown task for baoding: ", task)
+        self.rsi = enable_rsi
+        self.drop_th = drop_th
+        self.proximity_th = proximity_th
+        self.goal_time_period = goal_time_period
+        self.goal_xrange = goal_xrange
+        self.goal_yrange = goal_yrange
+
+        # balls start at these angles
+        #   1= yellow = top right
+        #   2= pink = bottom left
+        self.ball_1_starting_angle = 3.0 * np.pi / 4.0
+        self.ball_2_starting_angle = -1.0 * np.pi / 4.0
+
+        # init desired trajectory, for rotations
+        self.center_pos = [-0.0125, -0.07]  # [-.0020, -.0522]
+        self.x_radius = self.np_random.uniform(
+            low=self.goal_xrange[0], high=self.goal_xrange[1]
+        )
+        self.y_radius = self.np_random.uniform(
+            low=self.goal_yrange[0], high=self.goal_yrange[1]
+        )
+
+        self.counter = 0
+        self.goal = self.create_goal_trajectory(
+            time_step=frame_skip * self.sim.model.opt.timestep, time_period=6
+        )
+
+        # init target and body sites
+        self.object1_sid = self.sim.model.site_name2id("ball1_site")
+        self.object2_sid = self.sim.model.site_name2id("ball2_site")
+        self.object1_gid = self.sim.model.geom_name2id("ball1")
+        self.object2_gid = self.sim.model.geom_name2id("ball2")
+        self.target1_sid = self.sim.model.site_name2id("target1_site")
+        self.target2_sid = self.sim.model.site_name2id("target2_site")
+        self.sim.model.site_group[self.target1_sid] = 2
+        self.sim.model.site_group[self.target2_sid] = 2
+
+        BaseV0._setup(
+            self,
+            obs_keys=obs_keys,
+            weighted_reward_keys=weighted_reward_keys,
+            frame_skip=frame_skip,
+            **kwargs,
+        )
+
+        # reset position
+        self.init_qpos[:-14] *= 0  # Use fully open as init pos
+        self.init_qpos[0] = -1.57  # Palm up
