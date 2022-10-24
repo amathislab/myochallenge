0a09ff9886ed480ebe93e2fe8deb2af056661abc
Alberto Chiappa <alberto.chiappa@epfl.ch>
2022-10-24 10:43:11 +0200

    Add the new env to the factory and format baoding

--- src/envs/environment_factory.py
+++ src/envs/environment_factory.py
@@ -38,6 +38,8 @@ class EnvironmentFactory:
             return gym.make("CustomMyoChallengeBaodingP1-v1", **kwargs)
         elif env_name == "CustomMyoReorientP1":
             return gym.make("CustomMyoChallengeDieReorientP1-v0", **kwargs)
+        elif env_name == "MyoBaodingBallsP2":
+            return gym.make("myoChallengeBaodingP2-v1", **kwargs)
                    
     @staticmethod
     def register(env_name, **kwargs):
