c5c8305826fe6b344e388c1027085390c325add7
nisheetpatel <nisheet.pat@gmail.com>
2022-11-06 20:09:58 +0100

    fixed imports, removed src, and added init for package recognition

--- src/metrics/callbacks.py
+++ src/metrics/callbacks.py
@@ -1,9 +1,10 @@
+from typing import Dict, Optional
+
 import numpy as np
 from ray.rllib.agents.callbacks import DefaultCallbacks
-from typing import Dict, Optional
 from ray.rllib.env import BaseEnv
-from ray.rllib.policy import Policy
 from ray.rllib.evaluation import MultiAgentEpisode
+from ray.rllib.policy import Policy
 from ray.rllib.utils.typing import PolicyID
 
 
@@ -17,13 +18,15 @@ class FullInfoCallbacks(DefaultCallbacks):
     {"callbacks": YourCallbacksClass} in the trainer config.
     """
 
-    def on_episode_step(self,
-                        *,
-                        worker,
-                        base_env: BaseEnv,
-                        episode: MultiAgentEpisode,
-                        env_index: Optional[int] = None,
-                        **kwargs) -> None:
+    def on_episode_step(
+        self,
+        *,
+        worker,
+        base_env: BaseEnv,
+        episode: MultiAgentEpisode,
+        env_index: Optional[int] = None,
+        **kwargs,
+    ) -> None:
         """Runs on each episode step.
 
         Args:
@@ -49,15 +52,16 @@ class FullInfoCallbacks(DefaultCallbacks):
                 else:
                     episode.user_data.update({key: [value]})
 
-
-    def on_episode_end(self,
-                       *,
-                       worker,
-                       base_env: BaseEnv,
-                       policies: Dict[PolicyID, Policy],
-                       episode: MultiAgentEpisode,
-                       env_index: Optional[int] = None,
-                       **kwargs) -> None:
+    def on_episode_end(
+        self,
+        *,
+        worker,
+        base_env: BaseEnv,
+        policies: Dict[PolicyID, Policy],
+        episode: MultiAgentEpisode,
+        env_index: Optional[int] = None,
+        **kwargs,
+    ) -> None:
         """Runs when an episode is done.
 
         Args:
@@ -76,17 +80,22 @@ class FullInfoCallbacks(DefaultCallbacks):
         """
 
         if self.legacy_callbacks.get("on_episode_end"):
-            self.legacy_callbacks["on_episode_end"]({
-                "env": base_env,
-                "policy": policies,
-                "episode": episode,
-            })
-            
-        episode.custom_metrics.update({
-            f"{key}_mean": np.mean(values) for key, values in episode.user_data.items()
-        })
-        
-        
+            self.legacy_callbacks["on_episode_end"](
+                {
+                    "env": base_env,
+                    "policy": policies,
+                    "episode": episode,
+                }
+            )
+
+        episode.custom_metrics.update(
+            {
+                f"{key}_mean": np.mean(values)
+                for key, values in episode.user_data.items()
+            }
+        )
+
+
 class MyoInfoCallbacks(DefaultCallbacks):
     """Abstract base class for RLlib callbacks (similar to Keras callbacks).
 
@@ -97,13 +106,15 @@ class MyoInfoCallbacks(DefaultCallbacks):
     {"callbacks": YourCallbacksClass} in the trainer config.
     """
 
-    def on_episode_step(self,
-                        *,
-                        worker,
-                        base_env: BaseEnv,
-                        episode: MultiAgentEpisode,
-                        env_index: Optional[int] = None,
-                        **kwargs) -> None:
+    def on_episode_step(
+        self,
+        *,
+        worker,
+        base_env: BaseEnv,
+        episode: MultiAgentEpisode,
+        env_index: Optional[int] = None,
+        **kwargs,
+    ) -> None:
         """Runs on each episode step.
 
         Args:
@@ -118,23 +129,26 @@ class MyoInfoCallbacks(DefaultCallbacks):
                 episode belongs to.
             kwargs: Forward compatibility placeholder.
         """
-        
+
         metrics = ["rwd_dense", "rwd_sparse", "solved"]
         info = episode.last_info_for()
         if info is not None:
             reward_info = info["rwd_dict"]
             self.create_or_update_keys(episode.user_data, info, metrics)
-            self.create_or_update_keys(episode.user_data, reward_info, list(reward_info.keys()))
+            self.create_or_update_keys(
+                episode.user_data, reward_info, list(reward_info.keys())
+            )
 
-
-    def on_episode_end(self,
-                       *,
-                       worker,
-                       base_env: BaseEnv,
-                       policies: Dict[PolicyID, Policy],
-                       episode: MultiAgentEpisode,
-                       env_index: Optional[int] = None,
-                       **kwargs) -> None:
+    def on_episode_end(
+        self,
+        *,
+        worker,
+        base_env: BaseEnv,
+        policies: Dict[PolicyID, Policy],
+        episode: MultiAgentEpisode,
+        env_index: Optional[int] = None,
+        **kwargs,
+    ) -> None:
         """Runs when an episode is done.
 
         Args:
@@ -153,20 +167,23 @@ class MyoInfoCallbacks(DefaultCallbacks):
         """
 
         if self.legacy_callbacks.get("on_episode_end"):
-            self.legacy_callbacks["on_episode_end"]({
-                "env": base_env,
-                "policy": policies,
-                "episode": episode,
-            })
-            
+            self.legacy_callbacks["on_episode_end"](
+                {
+                    "env": base_env,
+                    "policy": policies,
+                    "episode": episode,
+                }
+            )
+
         # metrics = ["rwd_dense", "rwd_sparse"]
-        episode.custom_metrics.update({
-            f"{key}": np.mean(episode.user_data[key]) for key in episode.user_data.keys()
-        })
-        episode.custom_metrics.update({
-            "solved": episode.user_data["solved"][-1]
-        })
-        
+        episode.custom_metrics.update(
+            {
+                f"{key}": np.mean(episode.user_data[key])
+                for key in episode.user_data.keys()
+            }
+        )
+        episode.custom_metrics.update({"solved": episode.user_data["solved"][-1]})
+
     def create_or_update_keys(self, target_dict, input_dict, input_keys):
         for key in input_keys:
             if key in target_dict:
