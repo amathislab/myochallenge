ee648a5b9459c65d130ba1e4175ac3b38bfdb7b9
Alberto Chiappa <alberto.chiappa@epfl.ch>
2022-12-22 10:14:54 +0100

    Create tensorboard callback

--- src/metrics/custom_callbacks.py
+++ src/metrics/custom_callbacks.py
@@ -59,3 +59,23 @@ class EnvDumpCallback(BaseCallback):
             print("Saving the training environment to path ", env_path)
         self.training_env.save(env_path)
         return True
+    
+    
+class TensorboardCallback(BaseCallback):
+    def __init__(self, info_keywords, verbose=0):
+        super().__init__(verbose=verbose)
+        self.info_keywords = info_keywords
+        self.rollout_info = {}
+        
+    def _on_rollout_start(self):
+        self.rollout_info = {key: [] for key in self.info_keywords}
+        
+    def _on_step(self):
+        for key in self.info_keywords:
+            vals = [info[key] for info in self.locals["infos"]]
+            self.rollout_info[key].extend(vals)
+        return True
+    
+    def _on_rollout_end(self):
+        for key in self.info_keywords:
+            self.logger.record("rollout/" + key, np.mean(self.rollout_info[key]))
